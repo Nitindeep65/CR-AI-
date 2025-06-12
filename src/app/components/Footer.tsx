f82f12503407d5c2d468f7dfb0698b562bb8a3c5
Nitindeep Singh <nittin@Nitindeeps-MacBook-Air.local>
2025-06-12 14:57:10 +0530

    footer changed

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,10 +7,10 @@ const Footer = () => {
     <footer className="bg-gray-100 dark:bg-gray-900 border-t py-6 ">
       <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center text-center md:text-left space-y-4 md:space-y-0 text-sm text-gray-600 dark:text-gray-400">
         <p>
-          &copy; {new Date().getFullYear()} <span className="font-semibold text-blue-600 dark:text-blue-400">SmartReview AI</span>. Built with ❤️ using the MERN stack.
+          &copy; {new Date().getFullYear()} <span className="font-semibold text-blue-600 dark:text-blue-400">SmartReview AI</span>. Built By Nitindeep Singh 
         </p>
         <a
-          href="https://github.com"
+          href="https://github.com/Nitindeep65"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center space-x-2 hover:text-blue-600 dark:hover:text-blue-400 transition"
